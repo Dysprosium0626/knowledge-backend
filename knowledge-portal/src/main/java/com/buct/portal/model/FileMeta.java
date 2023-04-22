c12867ee748d6326e717d6e33eb5f0701b12b8c5
dysprosium0626 <1119493091@qq.com>
2023-04-22 23:55:47 +0800

    Revert "feat: add object storage support"
    This reverts commit 1a6892e98409c7af4a381711af3c06357de075c7.

--- knowledge-portal/src/main/java/com/buct/portal/model/FileMeta.java
+++ knowledge-portal/src/main/java/com/buct/portal/model/FileMeta.java
@@ -4,11 +4,8 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 /**
  * <p>
@@ -21,8 +18,6 @@ import lombok.NoArgsConstructor;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @TableName("file_meta")
-@AllArgsConstructor
-@NoArgsConstructor
 public class FileMeta implements Serializable {
 
     private static final long serialVersionUID = 1L;
